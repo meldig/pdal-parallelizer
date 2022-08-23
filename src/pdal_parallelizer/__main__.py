ab915930a3cfa802300e5e59bfaa5d12301121eb
ClementAlba <93247842+ClementAlba@users.noreply.github.com>
2022-08-23 15:21:07 +0200

    1.10.15

--- src/pdal_parallelizer/__main__.py
+++ src/pdal_parallelizer/__main__.py
@@ -19,7 +19,7 @@ import gc
 
 
 @click.group()
-@click.version_option('1.10.14')
+@click.version_option('1.10.15')
 def main():
     """A simple parallelization tool for 3d point clouds treatment"""
     pass
